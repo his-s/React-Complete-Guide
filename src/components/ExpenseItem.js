8dd9c429ec993d81d2d0fb495b023a8d547cf410
Hisham Elsayed <70527079+his-s@users.noreply.github.com>
2022-03-16 16:29:28 +0200

    split expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
+import ExpenseDate from "./ExpenseDate";
+const ExpenseItem = ({ props }) => {
   return (
     <div className="expense-item">
-      <div>March 28th 2020</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car incurance</h2>
-        <div className="expense-item__price">228.22$</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
